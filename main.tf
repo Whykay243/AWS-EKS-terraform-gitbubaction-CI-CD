558ebb95cefe208990f13e97f44359b4857be5ff
Whykay243 <yinka.ajibola0011@gmail.com>
2025-08-09 16:13:38 -0400

    updated pipeline

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "eks" {
   version = "20.36.0"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.31"
+  cluster_version = "1.33"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
