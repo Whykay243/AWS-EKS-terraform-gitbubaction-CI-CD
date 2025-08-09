e81f952fdbd1f10f3450cecdd2d3d71ae34ad58c
Yinka M Ajibola <yinka.ajibola0011@gmail.com>
2025-08-09 15:18:40 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket = "whykay-backend-bucket"
     key    = "eks/terraform.tfstate"
     region = "us-east-1"
-    profile = "whykay"  # Use the AWS profile configured in provider.tf
     # Optional:
     # encrypt = true
     # dynamodb_table = "terraform-lock-table"  # for state locking
