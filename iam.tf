3b59b97c762750c117a7b4ed5532544d9b599aa6
Whykay243 <yinka.ajibola0011@gmail.com>
2025-08-10 21:28:59 -0400

    new

--- iam.tf
+++ iam.tf
@@ -23,3 +23,44 @@ resource "aws_iam_role_policy_attachment" "eks_service_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
+
+
+# Fetch EKS cluster info for OIDC URL
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+# Create OIDC provider resource with cluster OIDC URL
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da0afd7e91d"]  # EKS thumbprint
+  url             = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
+
+# Create IAM Role for AWS Load Balancer Controller with trust policy to OIDC provider
+resource "aws_iam_role" "alb_controller" {
+  name = "alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+# Attach the AWS managed policy for ALB controller permissions
+resource "aws_iam_policy_attachment" "alb_controller_policy_attach" {
+  name       = "alb-controller-policy-attachment"
+  roles      = [aws_iam_role.alb_controller.name]
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+}
